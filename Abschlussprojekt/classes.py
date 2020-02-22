3bfcb900ce78cc520a6303bf84fda2c75cf609eb
Ludwig Wolfgruber <ludwig.wolfgruber@gmx.at>
2020-02-22 15:19:59 +0100

    Added two more plots, a few methods and some labels.

--- Abschlussprojekt/classes.py
+++ Abschlussprojekt/classes.py
@@ -28,7 +28,7 @@ class Data:
                     if i == 0:
                         self.lat = msg.get('distinctLatitudes')
                         self.lon = msg.get('distinctLongitudes')
-                        self.name = msg.get('name')
+                        self.name = msg.get('name')+' / '+msg.get('units')
                         self.values = np.empty(
                                 (n, len(self.lat), len(self.lon)))
                         
@@ -47,6 +47,13 @@ class Data:
 
     def comp_mean_time(self):
         return np.mean(self.values, axis=0)
+    
+    
+    def ann_mean(self, anno):
+        time_idx = (anno - 1979) * 12 - 1
+        data = np.mean(self.values[time_idx:time_idx+12,:,:], axis=0)
+        
+        return data
 
 
     def get_time_mean(self):
@@ -73,6 +80,23 @@ class Data:
 
     def get_name(self):
         return self.name
+    
+    
+    def reduce_grid(self, inpt, lon_min, lon_max, lat_min, lat_max):
+        '''Returns the data of inpt reduced to a grid with the borders lon_min,
+        lon_max, lat_min, lat_max'''
+
+        idx_lon_min = np.argmin(np.abs(self.lon - lon_min))
+        idx_lon_max = np.argmin(np.abs(self.lon - lon_max))
+        idx_lat_min = np.argmin(np.abs(self.lat - lat_min))
+        idx_lat_max = np.argmin(np.abs(self.lat - lat_max))
+
+        data = inpt[idx_lat_max:idx_lat_min,idx_lon_min:idx_lon_max]
+        lat = self.lat[idx_lat_max:idx_lat_min]
+        lon = self.lon[idx_lon_min:idx_lon_max]
+        
+        return (data, lat, lon)
+        
 
     
 def plot_mean_global(data1, data2):
@@ -98,8 +122,10 @@ def plot_mean_global(data1, data2):
                  hatches=['/', 'x','//', 'xx', '///', 'xxx', '////', 'xxxx'],
                  transform=ctp.crs.PlateCarree())
         
-    fig.colorbar(m1, orientation='horizontal')
-    fig.colorbar(m2, orientation='horizontal')
+    cbar1 = fig.colorbar(m1, orientation='horizontal')
+    cbar1.ax.set_xlabel(name1)
+    cbar2 = fig.colorbar(m2, orientation='horizontal')
+    cbar2.ax.set_xlabel(name2)
     ax1.coastlines()
     ax1.set_global()
     plt.savefig('fig/mean_global.png')
@@ -128,11 +154,81 @@ def plot_time_series(dataobject1, dataobject2):
     ax2.set_ylabel(name2, color=color)
     ax2.tick_params(axis='y', labelcolor=color)
     
+    plt.savefig('fig/time_series.png')
+    plt.show()
+    return
+
+def plot_austria(data1, data2, anno):
+    map_border = [9, 17.5, 46, 49.5]
+    values1, lat1, lon1 = data1.reduce_grid(data1.ann_mean(anno), *map_border)
+    name1 = data1.get_name()
+    values2, lat2, lon2 = data2.reduce_grid(data2.ann_mean(anno), *map_border)
+    name2 = data2.get_name()
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1, projection=ctp.crs.EuroPP())
+    ax1.set_extent(map_border, crs=ctp.crs.PlateCarree())
+        
+    m1 = ax1.contourf(lon1, lat1, values1, 14, transform=ctp.crs.PlateCarree(),
+                cmap='nipy_spectral')
+    
+    m2 = ax1.contourf(lon2, lat2, values2, colors='none',
+                 hatches=['/', 'x','//', 'xx', '///', 'xxx', '////', 'xxxx'],
+                 transform=ctp.crs.PlateCarree())
+        
+    ax1.add_feature(ctp.feature.BORDERS.with_scale('10m'))
+    cbar1 = fig.colorbar(m1, orientation='horizontal')
+    cbar1.ax.set_xlabel(name1)
+    cbar2 = fig.colorbar(m2, orientation='horizontal')
+    cbar2.ax.set_xlabel(name2)
+    ax1.coastlines()
+    plt.savefig('fig/austria_'+str(anno)+'.png')
+    plt.show()
+    return
+    
+    
+def plot_anomaly(data1, data2, anno):
+    lat1, lon1 = data1.get_coord()
+    mean1 = data1.get_time_mean()
+    name1 = data1.get_name()
+    
+    mean2 = data2.get_time_mean()
+    name2 = data2.get_name()
+    if mean1.shape == mean2.shape:
+        lat2 = lat1
+        lon2 = lon1
+    else:
+        lat2, lon2 = data2.get_coord()
+        
+    values1 = data1.ann_mean(anno)
+    values2 = data2.ann_mean(anno)
+    values1 = mean1 - values1
+    values2 = mean2 - values2
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1, projection=ctp.crs.Mollweide())
+        
+    m1 = ax1.contourf(lon1, lat1, values1, 15, transform=ctp.crs.PlateCarree(),
+                cmap='nipy_spectral')
+    
+    m2 = ax1.contourf(lon2, lat2, values2, colors='none',
+                 hatches=['/', 'x','//', 'xx', '///', 'xxx', '////', 'xxxx'],
+                 transform=ctp.crs.PlateCarree())
+        
+    cbar1 = fig.colorbar(m1, orientation='horizontal')
+    cbar1.ax.set_xlabel(name1)
+    cbar2 = fig.colorbar(m2, orientation='horizontal')
+    cbar2.ax.set_xlabel(name2)
+    ax1.coastlines()
+    ax1.set_global()
+    plt.savefig('fig/anomaly_'+str(anno)+'.png')
     plt.show()
     return
     
 if __name__ == '__main__':
-    perc = Data('data/data.grib', 'tp')
-    temp = Data('data/data.grib', '2t')
-    plot_mean_global(perc, temp)
+    perc = Data('data/data2.grib', 'tp')
+    temp = Data('data/data2.grib', '2t')
+    #plot_mean_global(perc, temp)
     #plot_time_series(perc, temp)
+    #plot_austria(temp, perc, 1980)
+    plot_anomaly(perc, temp, 1980)
